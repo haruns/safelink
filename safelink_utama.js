b254d86ac7e59c04975429a3f4f540c9d5c8d328
haruns <44577208+haruns@users.noreply.github.com>
2018-11-02 12:15:35 +0800

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*17)+ 1);
 var xxx=null;
 if(x=="1"){xxx="why-is-house-considered-real-estate.html"}
 if(x=="2"){xxx="reasons-some-rent-instead-of-own.html"}
